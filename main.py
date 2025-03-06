4551b278744240935b967ae5b4fac863f7558050
trh-dt <terekh.d08@gmal.com>
2025-03-06 17:34:08 +0100

    add new cod

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.types import WebAppInfo
+from aiogram.filters import Command  # Новый импорт
 
 TOKEN = "7913005358:AAG-SomQHx37Z4xBFVy5KyufSskFhYA2Y9w"
 
@@ -9,18 +10,18 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 # Команда /start
-@dp.message(commands=['start'])
+@dp.message(Command("start"))  # <-- Исправленный фильтр
 async def start(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     button = types.KeyboardButton(
         text="🛍 Открыть магазин",
-        web_app=WebAppInfo(url="https://ТВОЙ_GITHUB_НИК.github.io/oscuro-tienda/")
+        web_app=WebAppInfo(url="https://trh-dt.github.io/Oscuro_Tienda/index.html")
     )
     keyboard.add(button)
     await message.answer("Привет! Добро пожаловать в Oscuro Tienda!", reply_markup=keyboard)
 
 # Inline-кнопка (если нужна встроенная)
-@dp.message(commands=['shop'])
+@dp.message(Command("shop"))  # <-- Исправленный фильтр
 async def shop(message: types.Message):
     keyboard = types.InlineKeyboardMarkup()
     button = types.InlineKeyboardButton(
@@ -33,7 +34,7 @@ async def shop(message: types.Message):
 async def main():
     logging.basicConfig(level=logging.INFO)
     await bot.delete_webhook(drop_pending_updates=True)
-    dp.include_router(dp)
+    dp.include_router(dp)  # <-- Убедись, что этот вызов добавляет роутеры
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
