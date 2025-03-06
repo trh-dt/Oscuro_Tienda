616187d6e34def58738514d31a7d6faa8175c815
trh-dt <terekh.d08@gmal.com>
2025-03-06 17:42:28 +0100

    add new cod

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher, types, F
+from aiogram import Bot, Dispatcher, types
 from aiogram.types import WebAppInfo
-from aiogram.filters import Command  # Новый импорт
+from aiogram.filters import Command
 
 TOKEN = "7913005358:AAG-SomQHx37Z4xBFVy5KyufSskFhYA2Y9w"
 
@@ -10,25 +10,24 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 # Команда /start
-@dp.message(Command("start"))  # <-- Исправленный фильтр
+@dp.message(Command("start"))
 async def start(message: types.Message):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button = types.KeyboardButton(
-        text="🛍 Открыть магазин",
-        web_app=WebAppInfo(url="https://trh-dt.github.io/Oscuro_Tienda/index.html")
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=[  # Добавляем список кнопок
+            [types.KeyboardButton(text="🛍 Открыть магазин", web_app=WebAppInfo(url="https://trh-dt.github.io/Oscuro_Tienda/index.html"))]
+        ],
+        resize_keyboard=True
     )
-    keyboard.add(button)
     await message.answer("Привет! Добро пожаловать в Oscuro Tienda!", reply_markup=keyboard)
 
 # Inline-кнопка (если нужна встроенная)
-@dp.message(Command("shop"))  # <-- Исправленный фильтр
+@dp.message(Command("shop"))
 async def shop(message: types.Message):
-    keyboard = types.InlineKeyboardMarkup()
-    button = types.InlineKeyboardButton(
-        text="🛍 Перейти в магазин",
-        web_app=WebAppInfo(url="https://trh-dt.github.io/Oscuro_Tienda/index.html")
+    keyboard = types.InlineKeyboardMarkup(
+        inline_keyboard=[  # Теперь передаём список inline-кнопок
+            [types.InlineKeyboardButton(text="🛍 Перейти в магазин", web_app=WebAppInfo(url="https://trh-dt.github.io/Oscuro_Tienda/index.html"))]
+        ]
     )
-    keyboard.add(button)
     await message.answer("Добро пожаловать в Oscuro Tienda!", reply_markup=keyboard)
 
 async def main():
